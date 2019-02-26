95b42381e302eee9bdc6daffb643e2bd4053873b
Martin 123 <martin.jalid@123seguro.com.ar>
2019-02-26 12:36:34 -0300

    fix

--- challenge-n-2/start.py
+++ challenge-n-2/start.py
@@ -10,8 +10,8 @@ import bcrypt
 import configparser
 import smtplib
 import email
-# from email.MIMEMultipart import MIMEMultipart
-# from email.MIMEText import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from ldap3 import Server, Connection, ALL, NTLM
 
 config = configparser.ConfigParser()
@@ -19,6 +19,7 @@ config.read('config.ini')
 
 LDAP = config['LDAP']
 DATABASE = config['DATABASE']
+GMAIL = config['GMAIL']
 
 def loginGmail():
 	try:
@@ -26,17 +27,16 @@ def loginGmail():
 		gmail.ehlo()
 		gmail.starttls()
 		gmail.ehlo()
-		gmail.login("martinjalid@gmail.com", "622033940")
-		msg = "Hello!" # The /n separates the message from the headers
-		gmail.sendmail("martinjalid@gmail.com", "martin.jalid@123seguro.com.ar", msg)
-		print(gmail)
+		gmail.login(GMAIL["ACCOUNT"], GMAIL['PASSWORD'])
+		return gmail;
 	except Exception as e:
 		print(e)
 
 def connectLdap():
 	try:
 		server = Server(LDAP['HOST'], get_info=ALL)
-		conn = Connection(server, 'cn=admin,dc=meli,dc=com', password=LDAP['PASSWORD'], auto_bind=True)
+		connLdap = Connection(server, 'cn=admin,dc=meli,dc=com', password=LDAP['PASSWORD'], auto_bind=True)
+		return connLdap
 	except Exception as e:
 		print(e)
 
@@ -55,9 +55,10 @@ def generatePassword():
 
 def createDatabase():
 	try:
+	    cursor.execute("DROP DATABASE challenge2")
 	    cursor.execute("CREATE DATABASE IF NOT EXISTS challenge2")
 	    cursor.execute("USE challenge2")
-	    cursor.execute("CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(50), Surname VARCHAR(50), mail VARCHAR(100))")
+	    cursor.execute("CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(50), surname VARCHAR(50), mail VARCHAR(100), password VARCHAR(100))")
 	except Exception as e:
 	    print(e)
 
@@ -75,36 +76,55 @@ def parseCsv():
 
 def createUsers(users):
 	for user in users:
-		createUserInDB(user)
+		password = generatePassword()
+		
+		user['Password'] = password
+		# createUserInDB(user)
 		createUserInLDAP(user)
-		# sendMail(user['Mail'])
+		# sendMail(user)
 
 def createUserInDB(user):
-	print(user, 'DB')
+	try:
+		hashed = bcrypt.hashpw(user['Password'], bcrypt.gensalt())
+		sql = "INSERT INTO users(name, surname, mail, password) VALUES(%s, %s, %s, %s)"
+		values = (user['Name'], user['Surname'], user['Mail'], hashed)
+		cursor.execute(sql, values)
+		conn.commit()
+	except Exception as e:
+		print(e)
 
 def createUserInLDAP(user):
-	print(user, 'LADP')
+	username = 'john.smith'
+	useremail = 'john@smith.com'
+	userpswd = 'AbcDef$$1234567'
+	userdn = 'CN=john.smith,OU=Users,DC=directory,DC=local'
 
-def sendMail(mail):
-	fromaddr = "martinjalid@gmail.com"
-	toaddr = "martin.jalid@123seguro.com.ar"
+	connLdap.add(userdn, attributes={
+	   'objectClass': ['organizationalPerson', 'person', 'top', 'user'],
+	   'sAMAccountName': username,
+	   'userPrincipalName': "{}@{}".format(username, domain),
+	   'displayName': username,
+	   'mail': useremail  # optional
+	})
+
+	result = connLdap.add_s(dn, ldap.modlist.addModlist(modlist))
+	print(connLdap.result)
+
+def sendMail(user):
+	fromaddr = GMAIL['ACCOUNT']
+	toaddr = user['Mail']
 	msg = MIMEMultipart()
 	msg['From'] = fromaddr
 	msg['To'] = toaddr
-	msg['Subject'] = "Python email"
-	body = "Python test mail"
+	msg['Subject'] = "Bienvenido a MercadoLibre!"
+	body = "Su nuevo usuario es: {} \nLa contraseña para ingresar: {}".format(user['Name'], user['Password'])
 	msg.attach(MIMEText(body, 'plain'))
 	text = msg.as_string()
 	gmail.sendmail(fromaddr, toaddr, text)
-	print('Mail sended to: '+ mail)
+	print(gmail)
 
-# loginGmail()
-# createDatabase()
-connectLdap()
-
+gmail = loginGmail()
+createDatabase()
+connLdap = connectLdap()
 users = parseCsv()
 createUsers(users)
-
-password = generatePassword()
-
-hashed = bcrypt.hashpw(password, bcrypt.gensalt())
